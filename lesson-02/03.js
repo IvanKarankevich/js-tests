eabd802ddf59bff445065ac64d6384e90c7739c3
IvanKarankevich <vancha224@gmail.com>
2025-08-03 22:06:16 +0300

    second lesson task 3 try 1

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -6,4 +6,19 @@
 
 const score = 70 // тестовое значение, можно изменять
 let grade
+if (score <=49){
+    grade = 'F'
+}
+else if ((score >= 50) && (score <= 69)){
+    grade = 'D'
+}  
+else if ((score >= 70) && (score <= 79)){
+    grade = 'C'
+}
+else if ((score >= 80) && (score <= 89)){
+    grade = 'B'
+}
+else {
+    grade = 'A'
+}      
 // your code
